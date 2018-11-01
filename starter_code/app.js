ebedc72ea69647bfa5734ad0802041d50971adfc
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-01 10:00:35 +0100

    90% Finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,18 +3,54 @@ const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
 const punkAPI = new PunkAPIWrapper();
+const beer1 = punkAPI.getBeer(1)
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// const beerCheck = document.getElementsByClassName('beer-check')
+
+// beerCheck.onclick = function(){
+
+// }
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(rand => {
+    console.log('random')
+
+    res.render('random-beers', {rand});
+    
+  })
+  .catch(error=>{
+    console.log('error')
+  })
+});
+
+
+app.get('/beers', (req, res, next) => {
+
+
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log('are there beers')
+
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
 
 
 
